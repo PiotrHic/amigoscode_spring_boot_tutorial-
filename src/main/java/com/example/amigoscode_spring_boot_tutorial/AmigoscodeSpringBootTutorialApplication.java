8d83eea867d29de987024f84b9954818620bc151
piotrhic <piotrhic@gmail.com>
2023-02-21 00:05:35 +0100

    Create Simple API #2.

--- src/main/java/com/example/amigoscode_spring_boot_tutorial/AmigoscodeSpringBootTutorialApplication.java
+++ src/main/java/com/example/amigoscode_spring_boot_tutorial/AmigoscodeSpringBootTutorialApplication.java
@@ -2,12 +2,22 @@ package com.example.amigoscode_spring_boot_tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class AmigoscodeSpringBootTutorialApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmigoscodeSpringBootTutorialApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public List<String> hello(){
+        return List.of("Hello", "World");
     }
 
 }
